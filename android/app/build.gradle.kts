9e8f5b6efd551ff98181ca71eea96e84ad5f9ef7
iCloud Solutions <contact@icloud-solution.net>
2025-08-25 09:36:59 +0100

    Initial commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.example.nawarabloom"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.2.12479018"
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+    //ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
